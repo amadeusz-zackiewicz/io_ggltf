cf09d98860fc5a21eadbcfcb9ab1da976e01cbe8
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2022-02-27 22:15:07 +0000

    Add functionality to write triangle indices to mesh

--- Scoops/scoop_mesh.py
+++ Scoops/scoop_mesh.py
@@ -33,33 +33,38 @@ def scoop_base_mesh(
 
 # how to generate names:
 #
-# mesh name:            <original.name>.<hex(depsgraph_ID)>
-# buffer name:          <original.name>.<hex(depsgraph_ID)>
-# buffer view name:     <original.name>.<hex(depsgraph_ID)>.<attribute>
-# accessor name:        <original.name>.<hex(depsgraph_ID)>.<attribute>
+# mesh name:            <original.name>.<hex(depsgraph_ID)>.<mode>
+# accessor name:        <original.name>.<hex(depsgraph_ID)>.<mode>.<primitive>.<attribute>
 #
 # if the attribute is not dynamic, for example POSITION or NORMAL, 
 # use the attribute type itself, for dynamic attributes such
 # as UV maps or weight use the same name as blender
 # for example:
-# cube.0x1234.POSITION or cube.0x1234.uv_map_final
+# cube.0x1234.0.4.POSITION or cube.0x1234.0.4.uv_map_final
 #
 # Q: why the hex()?
 # A: looks cooler then pure int
 
 def __scoop_triangles(bucket, meshObj, uvMaps, morphs, skin):
- 
+
     originalName = meshObj.original.name
     depsID = str(hex(id(meshObj)))
 
-    tracker_name = ".".join([originalName, depsID])
+    trackerName = ".".join([originalName, depsID, str(MESH_TYPE_TRIANGLES)])
 
-    if tracker_name in bucket.trackers[BUCKET_TRACKER_MESHES]:
-        return bucket.trackers[BUCKET_TRACKER_MESHES][tracker_name]
+    if trackerName in bucket.trackers[BUCKET_TRACKER_MESHES]:
+        return bucket.trackers[BUCKET_TRACKER_MESHES][trackerName]
 
+    meshObj.calc_loop_triangles()
+    meshObj.calc_normals_split()
+    meshObj.calc_normals()
 
     positions = []
     normals = []
+    indices = []
+
+    min = [-1, -1, -1]
+    max = [1, 1, 1]
 
     for v in meshObj.vertices:
         v = Util.location_ensure_coord_space(bucket, v.co)
@@ -69,12 +74,19 @@ def __scoop_triangles(bucket, meshObj, uvMaps, morphs, skin):
         v = Util.direction_ensure_coord_space(bucket, v.normal)
         normals.append(v)
 
+    
+    for l in meshObj.loop_triangles:
+        vertices = l.vertices
+        for v in vertices: # should always be 3
+            indices.append(v)
+
     accessors = {
         GEOMETRY_ATTRIBUTE_STR_POSITION: AccessorManager.add_accessor(bucket, 
         ACCESSOR_COMPONENT_TYPE_FLOAT, 
         ACCESSOR_TYPE_VECTOR_3,
         PACKING_FORMAT_FLOAT, 
-        positions),
+        positions, min, max
+        ),
         GEOMETRY_ATTRIBUTE_STR_NORMAL: AccessorManager.add_accessor(bucket,
         ACCESSOR_COMPONENT_TYPE_FLOAT, 
         ACCESSOR_TYPE_VECTOR_3, 
@@ -83,19 +95,29 @@ def __scoop_triangles(bucket, meshObj, uvMaps, morphs, skin):
         )
     }
 
-    mesh_dict = {
-        MESH_NAME: tracker_name,
+    indicesAccess = AccessorManager.add_accessor(bucket, 
+    ACCESSOR_COMPONENT_TYPE_UNSIGNED_INT,
+    ACCESSOR_TYPE_SCALAR,
+    PACKING_FORMAT_U_INT,
+    indices
+    )
+
+    meshDict = {
+        MESH_NAME: trackerName,
         MESH_PRIMITIVES:[{
             MESH_PRIMITIVE_ATTRIBUTES: accessors,
             MESH_PRIMITIVE_MODE: MESH_TYPE_TRIANGLES,
-            MESH_PRIMITIVE_MATERIAL: 0 #TODO: ensure materials are correctly assigned here
+            MESH_PRIMITIVE_MATERIAL: 0, #TODO: ensure materials are correctly assigned here
+            MESH_PRIMITIVE_INDICES: indicesAccess
         }]
         }
 
     meshID = len(bucket.data[BUCKET_DATA_MESHES])
 
-    bucket.data[BUCKET_DATA_MESHES].append(mesh_dict)
-    bucket.trackers[BUCKET_TRACKER_MESHES][tracker_name] = meshID
+    bucket.data[BUCKET_DATA_MESHES].append(meshDict)
+    bucket.trackers[BUCKET_TRACKER_MESHES][trackerName] = meshID
+
+    # TODO: accessors are still not tracked
 
     return meshID
     
