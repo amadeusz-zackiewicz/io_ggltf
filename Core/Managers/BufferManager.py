e65b600f96ff8f33e4810e1975316e071970be48
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2022-02-27 22:14:20 +0000

    Fix a bug where buffer byte length was incorrect

--- Core/Managers/BufferManager.py
+++ Core/Managers/BufferManager.py
@@ -1,8 +1,8 @@
 from io_advanced_gltf2.Keywords import *
 import base64
 
-__GLTF_GLB_PADDING = b"00"
-__GLTF_EMBEDDED_PADDING = "data:application/gltf-buffer;base64,"
+__GLTF_GLB_PADDING = b"\x00"
+__GLTF_EMBEDDED_PADDING = "data:application/octet-stream;base64,"
 __GLTF_EMBEDDED_PADDING_LENGTH = len(__GLTF_EMBEDDED_PADDING)
 __GLTF_GLB_PADDING_LENGTH = len(__GLTF_GLB_PADDING)
 
@@ -19,10 +19,10 @@ def add_bytes(bucket, bytes: bytearray):
 
     if len(bucket.data[BUCKET_DATA_BUFFERS]) == 0:
         bucket.data[BUCKET_DATA_BUFFERS].append({
-            BUFFER_URI: __get_uri(bucket, 0),
-            BUFFER_BYTE_LENGTH: 0
+            BUFFER_BYTE_LENGTH: 0,
+            BUFFER_URI: __get_uri(bucket, 0)
         })
-
+        bucket.trackers[BUCKET_TRACKER_BUFFER_PADS].append(0)
         bucket.blobs.append(bytearray())
 
     # TODO: buffers have a limit on how much they can contain
@@ -33,24 +33,30 @@ def add_bytes(bucket, bytes: bytearray):
     # for now everything is merged into a massive blob
     # ignoring the issue
     
-    buffer_obj = bucket.data[BUCKET_DATA_BUFFERS][0]
+    #buffer_obj = bucket.data[BUCKET_DATA_BUFFERS][0]
 
-    if bucket.settings[BUCKET_SETTING_FILE_TYPE] == FILE_TYPE_GLTF_EMBEDDED:
-        bytes = __into_base64(bytes)
+    #if bucket.settings[BUCKET_SETTING_FILE_TYPE] == FILE_TYPE_GLTF_EMBEDDED:
+        #bytes = __into_base64(bytes)
 
+    byteLength = len(bytes)
     bufferID = 0
     byteOffset = len(bucket.blobs[0])
-    byteLength = len(bytes)
-    bucket.blobs[0] += bytes
 
-    buffer_obj[BUFFER_BYTE_LENGTH] = len(bucket.blobs[0])
+    bucket.blobs[0] += bytes
+    padding = (4 - (byteLength % 4)) % 4
+    bytes += __GLTF_GLB_PADDING * padding
+
 
     return (bufferID, byteOffset, byteLength)
 
 def write_blobs(bucket):
     #TODO: write logic for all file types
-    for i, buffer in enumerate(bucket.data[BUCKET_DATA_BUFFERS]):
-        buffer[BUFFER_URI] += str(__into_base64(bucket.blobs[i]), "utf-8")
+    if bucket.settings[BUCKET_SETTING_FILE_TYPE] == FILE_TYPE_GLTF_EMBEDDED:
+        for i, buffer in enumerate(bucket.data[BUCKET_DATA_BUFFERS]):
+            buffer[BUFFER_BYTE_LENGTH] = len(bucket.blobs[i])
+            buffer[BUFFER_URI] += __into_base64(bucket.blobs[i]).decode("utf8")
+    else:
+        print("file type not yet supported")
 
 def __get_uri(bucket, id):
     setting = bucket.settings[BUCKET_SETTING_FILE_TYPE]
@@ -58,10 +64,10 @@ def __get_uri(bucket, id):
     if setting == FILE_TYPE_GLTF:
         return bucket.settings[BUCKET_SETTING_BINPATH] + str(id) + FILE_EXT_BIN
     if setting == FILE_TYPE_GLB:
-        return str(__GLTF_GLB_PADDING, "utf-8")
+        return str(__GLTF_GLB_PADDING, "utf8")
     if setting == FILE_TYPE_GLTF_EMBEDDED:
         return __GLTF_EMBEDDED_PADDING
 
     
 def __into_base64(bytes):
-    return base64.standard_b64encode(bytes)
+    return base64.b64encode(bytes)
