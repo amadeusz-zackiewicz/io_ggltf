569e011e2984732ec859261560b026ee0529368c
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2022-05-13 03:18:09 +0100

    Add smart redundancy check

--- Core/Managers/RedundancyManager.py
+++ Core/Managers/RedundancyManager.py
@@ -1,12 +1,23 @@
-from io_advanced_gltf2.Keywords import *
 from io_advanced_gltf2.Core.Bucket import Bucket
 
 
 def is_redundant(bucket: Bucket, obj):
-    return id(obj) in bucket.redundancies
+    evalObj = bucket.currentDependencyGraph.id_eval_get(obj)
+    return id(evalObj) in bucket.redundancies
 
-def set_predetermined_id(bucket: Bucket, obj, pID):
-    bucket.redundancies[id(obj)] = pID
+def add_redundancy(bucket: Bucket, obj, pID):
+    evalObj = bucket.currentDependencyGraph.id_eval_get(obj)
+    bucket.redundancies[id(evalObj)] = pID
 
-def get_predetermined_id(bucket: Bucket, obj):
-    return bucket.redundancies[id(obj)]
+def get_redundancy(bucket: Bucket, obj):
+    evalObj = bucket.currentDependencyGraph.id_eval_get(obj)
+    return bucket.redundancies[id(evalObj)]
+
+def auto_redundancy(bucket: Bucket, obj, bucketDataType):
+    if is_redundant(bucket, obj):
+        return (True, get_redundancy(bucket, obj))
+    else:
+        newID = len(bucket.preScoopCounts[bucketDataType])
+        bucket.preScoopCounts[bucketDataType]
+        add_redundancy(bucket, obj, newID)
+        return (False, newID)
