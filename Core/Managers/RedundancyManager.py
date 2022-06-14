231001f339996b8b3164a02db650b26b5a88d919
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2022-06-14 05:48:34 +0100

    Rename functions within RedundancyManager for clarity

--- Core/Managers/RedundancyManager.py
+++ Core/Managers/RedundancyManager.py
@@ -1,7 +1,6 @@
 from io_advanced_gltf2.Core.Bucket import Bucket
 
-
-def is_redundant(bucket: Bucket, obj):
+def is_redundant(bucket: Bucket, obj) -> bool:
     evalObj = bucket.currentDependencyGraph.id_eval_get(obj)
     return id(evalObj) in bucket.redundancies
 
@@ -9,15 +8,15 @@ def add_redundancy(bucket: Bucket, obj, pID):
     evalObj = bucket.currentDependencyGraph.id_eval_get(obj)
     bucket.redundancies[id(evalObj)] = pID
 
-def get_redundancy(bucket: Bucket, obj):
+def get_redundancy(bucket: Bucket, obj) -> int:
     evalObj = bucket.currentDependencyGraph.id_eval_get(obj)
     return bucket.redundancies[id(evalObj)]
 
-def auto_redundancy(bucket: Bucket, obj, bucketDataType):
+def smart_redundancy(bucket: Bucket, obj, bucketDataType):
     if is_redundant(bucket, obj):
         return (True, get_redundancy(bucket, obj))
     else:
-        newID = len(bucket.preScoopCounts[bucketDataType])
-        bucket.preScoopCounts[bucketDataType]
+        newID = bucket.preScoopCounts[bucketDataType]
+        bucket.preScoopCounts[bucketDataType] += 1
         add_redundancy(bucket, obj, newID)
         return (False, newID)
