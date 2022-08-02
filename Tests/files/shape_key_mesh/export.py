2e4be6ce1b8c26bf8eaabe050579c4be6dbbf7f7
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2022-08-02 19:38:20 +0100

    Add shape key mesh test

---
+++ Tests/files/shape_key_mesh/export.py
@@ -0,0 +1,34 @@
+# when a script is run in blender it is considered to be __main__ but
+# when we run this as a file using terminal there is only 1 argument and
+# when blender launches a python file it has 2 arguments
+# so if there is only 1 argument, we are running it as a standalone python file
+# otherwise blender is running it
+import sys
+if __name__ == "__main__" and len(sys.argv) == 1:
+    import os
+    sys.path.append(os.path.abspath(""))
+    import Tests.LimitedTestUtil as T
+    T.run_test("shape_key_mesh", "shape_key_mesh", os.path.basename(__file__).replace(".py", ""))
+else:
+    from io_advanced_gltf2.Advanced import *
+    from io_advanced_gltf2.Keywords import *
+
+    filePath = "//..\\..\\output\\"
+    fileName = "shape_key_mesh_export"
+    binPath = ""
+
+    def test(bucket):
+        node = Node.based_on_object(bucket, "Cube")
+        mesh = Mesh.based_on_object(bucket, "Cube",
+        normals=True,
+        tangents=False,
+        boneInfluences=False,
+        shapeKeys=["Key 1", "Key 2"],
+        shapeKeyNormals=True
+        )
+        Linker.mesh_to_node(bucket, mesh, node)
+        File.dump_bucket(bucket)
+
+    test(File.create_bucket(filePath, fileName + "_embedded", binPath, FILE_TYPE_GLTF_EMBEDDED))
+    test(File.create_bucket(filePath, fileName, binPath, FILE_TYPE_GLTF))
+    test(File.create_bucket(filePath, fileName, binPath, FILE_TYPE_GLB))
