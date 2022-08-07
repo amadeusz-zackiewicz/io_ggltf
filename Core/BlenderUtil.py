5510e0101c1aa98ff304e232d0365a28cdf2ab85
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2022-08-08 00:23:13 +0100

    Add more utility methods

--- Core/BlenderUtil.py
+++ Core/BlenderUtil.py
@@ -15,3 +15,33 @@ def set_object_modifier(bucket: Bucket, objAccessor, modifierID, setActive):
 
 def get_object_accessor(obj):
     return (obj.name, obj.library.filepath if obj.library != None else None)
+
+def object_has_uv_maps(obj, uvMaps: list[str]) -> bool:
+    for map in uvMaps:
+        if not map in obj.data.uv_layers:
+            return False
+    # all maps names were found
+    return True
+
+def object_has_vertex_colors(obj, vertexColors: list[str]) -> bool:
+    for vc in vertexColors:
+        if not vc in obj.data.vertex_colors:
+            return False
+    # all vertex colors names found
+    return True
+
+def object_has_shape_keys(obj, shapeKeys: list[str]) -> bool:
+    for key in shapeKeys:
+        if key not in obj.data.shape_keys.key_blocks:
+            return False
+    # all shape keys found
+    return True
+
+__disable_modifier_command = lambda bucket, objAccessor, modifierID: set_object_modifier(bucket=bucket, objAccessor=objAccessor, modifierID=modifierID, setActive=False)
+__enable_modifier_command = lambda bucket, objAccessor, modifierID: set_object_modifier(bucket=bucket, objAccessor=objAccessor, modifierID=modifierID, setActive=True)
+
+def queue_disable_modifier_type(bucket, obj, modType):
+    for i, mod in enumerate(obj.modifiers):
+        if mod.type == modType:
+            bucket.commandQueue[COMMAND_QUEUE_SETUP].append((__disable_modifier_command,(bucket, get_object_accessor(obj), i)))
+            bucket.commandQueue[COMMAND_QUEUE_CLEAN_UP].append((__enable_modifier_command, (bucket, get_object_accessor(obj), i)))
