91809c25affc25f46abd5923d170623fcac59708
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2022-09-21 03:47:18 +0100

    Add auto parenting to node module methods

--- Advanced/Node.py
+++ Advanced/Node.py
@@ -4,12 +4,11 @@ from io_ggltf.Core.Managers import RedundancyManager as RM
 from io_ggltf.Core.Bucket import Bucket
 from io_ggltf.Core.Util import try_get_object
 from io_ggltf.Core.BlenderUtil import get_object_accessor
-from io_ggltf.Core import Linker
-from io_ggltf.Advanced import Settings
+from io_ggltf.Advanced import Settings, Linker 
 from io_ggltf.Core import Util, BlenderUtil
 import bpy
 
-__linkChildCommand = lambda bucket, pID, cID: Linker.node_to_node(bucket=bucket, parentID=pID, childID=cID)
+#__linkChildCommand = lambda bucket, pID, cID: Linker.node_to_node(bucket=bucket, parentID=pID, childID=cID)
 __scoopCommand = lambda bucket, assignedID, objID, parent: NodeScoop.scoop_object(bucket=bucket, assignedID=assignedID, objAccessor=objID, parent=parent)
 
 def based_on_object(bucket: Bucket, objAccessor, parent=None, checkRedundancies=None, rename=None) -> int:
@@ -35,6 +34,9 @@ def based_on_object(bucket: Bucket, objAccessor, parent=None, checkRedundancies=
             raise Exception(f"based_on_object: 'rename' is expected to be a string, got {type(rename)} instead.")
         
     bucket.commandQueue[COMMAND_QUEUE_NODE].append((__scoopCommand, (bucket, nodeID, get_object_accessor(obj), parent)))
+
+    __auto_parent(bucket, obj, nodeID, parent)
+
     return nodeID
 
 
@@ -60,7 +62,7 @@ def based_on_hierarchy(bucket: Bucket, topObjAccessor, blacklist = {}, parent=No
             nodeID = RM.register_unsafe(bucket, get_object_accessor(obj), BUCKET_DATA_NODES)
 
         for c in childrenIDs:
-            bucket.commandQueue[COMMAND_QUEUE_LINKER].append((__linkChildCommand, (bucket, nodeID, c)))
+            Linker.node_to_node(bucket, c, nodeID)
         
         bucket.commandQueue[COMMAND_QUEUE_NODE].append((__scoopCommand, (bucket, nodeID, get_object_accessor(obj), parent)))
         return nodeID
@@ -72,10 +74,11 @@ def based_on_hierarchy(bucket: Bucket, topObjAccessor, blacklist = {}, parent=No
     if checkRedundancies == None:
         checkRedundancies = Settings.get_setting(bucket, BUCKET_SETTING_REDUNDANCY_CHECK_NODE)
 
-    if type(parent) != bool:
-        parObj = try_get_object(parent)
+    topNodeID = __recursive(bucket, obj, blacklist, parent, checkRedundancies, filters)
 
-    return __recursive(bucket, obj, blacklist, parent, checkRedundancies, filters)
+    __auto_parent(bucket, obj, topNodeID, parent)
+
+    return topNodeID
 
 
 def __object_is_collection_instance(obj) -> bool:
@@ -91,6 +94,8 @@ def __get_collection_top_objects(collection, blacklist={}):
 def based_on_collection(bucket: Bucket, collectionName, blacklist={}, parent=None, checkRedundancies=None, filters=[]) -> list:
     if checkRedundancies == None:
         checkRedundancies = Settings.get_setting(bucket, BUCKET_SETTING_REDUNDANCY_CHECK_NODE)
+    if parent == None:
+        parent = Settings.get_setting(bucket, BUCKET_SETTING_NODE_DEFAULT_PARENT_SPACE)
 
     collection = bpy.data.collections.get(collectionName)
 
@@ -102,3 +107,41 @@ def based_on_collection(bucket: Bucket, collectionName, blacklist={}, parent=Non
             nodeIDs.append(based_on_hierarchy(bucket, get_object_accessor(topObj), blacklist, parent, checkRedundancies, filters=filters))
 
     return nodeIDs
+
+def __auto_parent(bucket: Bucket, childObj, childID, parent):
+    if type(parent) == str:
+        try:
+            parentObj = Util.try_get_object(parent)
+        except:
+            return
+        parent = (parent, None) # this will trigger  == tuple below
+
+    if type(parent) == bool:
+        if parent:
+            if childObj.parent != None:
+                parent = BlenderUtil.get_parent_accessor(childObj) # this will trigger  == tuple below
+            else:
+                return
+        else:
+            return
+
+    if type(parent) == int:
+        parent = bucket.accessors[BUCKET_DATA_NODES][parent] # this will trigger  == tuple below
+
+    if type(parent) == tuple:
+        parentID = __get_parent_id(bucket, parent)
+        Linker.node_to_node(bucket, childID, parentID)
+        return
+
+    raise Exception(f"Failed to resolve parent for '{BlenderUtil.get_object_accessor(childObj)}', parent accessor was: '{parent}'. Make sure that parent is added before the child")
+
+def __get_parent_id(bucket: Bucket, accessor):
+    try:
+        id = RM.fetch_unique(bucket, accessor, BUCKET_DATA_NODES)
+        return id
+    except:
+        try:
+            id = RM.fetch_last_id_from_unsafe(bucket, accessor, BUCKET_DATA_NODES)
+            return id
+        except:
+            raise Exception(f"{accessor} needs to be added before it's children can access it")
