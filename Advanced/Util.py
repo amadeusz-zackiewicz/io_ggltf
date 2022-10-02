96e457a7d7e25a11f99e57e65e68e0e04520d8d1
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2022-10-02 12:29:39 +0100

    Add pattern name replacement methods in Advanced.Util module

--- Advanced/Util.py
+++ Advanced/Util.py
@@ -1,1 +1,13 @@
-from io_ggltf.Core.Util import create_filter
+from io_ggltf import Keywords as __k
+from io_ggltf.Core.Bucket import Bucket
+from io_ggltf.Core.Util import create_filter, pattern_replace as __replace
+
+def pattern_replace_node_names(bucket: Bucket, pattern: str, replacement: str):
+    bucket.commandQueue[__k.COMMAND_QUEUE_NAMING].append((__replace, (bucket, __k.BUCKET_DATA_NODES, pattern, replacement)))
+
+def pattern_replace_mesh_names(bucket: Bucket, pattern: str, replacement: str):
+    bucket.commandQueue[__k.COMMAND_QUEUE_NAMING].append((__replace, (bucket, __k.BUCKET_DATA_MESHES, pattern, replacement)))
+
+def pattern_replace_all_names(bucket: Bucket, pattern: str, replacement: str):
+    pattern_replace_node_names(bucket, pattern, replacement)
+    pattern_replace_mesh_names(bucket, pattern, replacement)
