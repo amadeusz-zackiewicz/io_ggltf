4297daaf40099fb595d35844fa6e4cb715432ab6
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2022-10-20 04:05:39 +0100

    Add Exceptions module

---
+++ Core/Exceptions/MeshExceptions.py
@@ -0,0 +1,24 @@
+class MissingMeshComponentException(Exception):
+    def __init__(self, objName: str, missing: list[str], context: str):
+        self.__objName = objName
+        self.__missing = missing
+        self.__context = context
+
+    def __str__(self):
+        return f"An exception has occured due to missing mesh components:\n{self.__context} from '{self.__objName}'{'\n\t'.join(self.__missing)} "
+
+class MissingUVMapsException(MissingMeshComponentException):
+    def __init__(self, objName: str, missing: list[str]):
+        super().__init__(objName=objName, missing=missing, context="Missing UV Maps")
+
+class MissingVertexColorsException(MissingMeshComponentException):
+    def __init__(self, objName: str, missing: list[str]):
+        super().__init__(objName=objName, missing=missing, context="Missing Vertex Colors")
+
+class MissingShapeKeysException(MissingMeshComponentException):
+    def __init__(self, objName: str, missing: list[str]):
+        super().__init__(objName, missing, context="Missing Shape Keys")
+
+class MeshComponentValidationFailedException(Exception):
+    def __str__(self):
+        return "Mesh validation has failed, check above for details"
---
+++ Core/Exceptions/__init__.py
@@ -0,0 +1,1 @@
+from io_ggltf.Core.Exceptions.MeshExceptions import *
