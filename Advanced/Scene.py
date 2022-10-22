845a90aa3695f5d1a484d0696f58b1c5e5ec01b6
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2022-10-22 05:09:43 +0100

    Modify the scene append to allow for multiple scene IDs

--- Advanced/Scene.py
+++ Advanced/Scene.py
@@ -11,12 +11,16 @@ def create(bucket: Bucket, name = None, nodes = []) -> int:
 
     return sceneID
 
-def append_node(bucket: Bucket, sceneID: int, nodeIDs: list or int):
+def append_node(bucket: Bucket, sceneIDs: list or int, nodeIDs: list or int):
+    if type(sceneIDs) == int:
+        sceneIDs = [sceneIDs]
+
     if type(nodeIDs) == int:
         nodeIDs = [nodeIDs]
 
-    scene = bucket.data[__c.BUCKET_DATA_SCENES][sceneID]
+    for sceneID in sceneIDs:
+        scene = bucket.data[__c.BUCKET_DATA_SCENES][sceneID]
 
-    for nodeID in nodeIDs:
-        if nodeID not in scene[__c.SCENE_NODES]:
-            scene[__c.SCENE_NODES].append(nodeID)
+        for nodeID in nodeIDs:
+            if nodeID not in scene[__c.SCENE_NODES]:
+                scene[__c.SCENE_NODES].append(nodeID)
