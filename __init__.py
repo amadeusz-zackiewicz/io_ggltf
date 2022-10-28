40c9cc2e46ab99c7dc1c6503b4ba85e7470898bb
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2022-10-28 08:51:13 +0100

    Update addon init information

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 bl_info = {
-    'name': 'gglTF (Game glTF) 2.0 Exporter',
+    'name': 'gglTF (Game glTF) Exporter',
     'author': 'Amadeusz Zackiewicz',
     "version": (0, 1, 0),
     'blender': (3, 3, 0),
-    'location': 'Scripting',
+    'location': 'Text Editor',
     'description': 'Export glTF 2.0 files and enjoy time saving functionality via python scripts',
     'warning': '',
     'doc_url': "https://github.com/amadeusz-zackiewicz/io_ggltf/wiki",
-    #'tracker_url': "",
+    'tracker_url': "https://github.com/amadeusz-zackiewicz/io_ggltf/issues",
     'support': 'COMMUNITY',
     'category': 'Import-Export',
 }
