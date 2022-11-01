5c399c026cea05e02b582c00a230376d9e388584
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2022-11-01 19:44:05 +0000

    Add more convienience buttons

--- Core/BlenderUI/Operators/TextEditorBoilerPlateOps.py
+++ Core/BlenderUI/Operators/TextEditorBoilerPlateOps.py
@@ -1,23 +1,63 @@
 import bpy
 
-class PasteBoilerPlateOperator(bpy.types.Operator):
+class PasteQuickStartOperator(bpy.types.Operator):
     """Paste quick start code"""
-    bl_idname = "ggltf.paste_adv_boiler_plate"
+    bl_idname = "ggltf.paste_adv_quick_start"
     bl_label = "Paste quick start code"
+    bl_button_label = "Quick start"
+    bl_button_icon = "FILE_TEXT"
 
     @classmethod
-    def poll(cls, context):
+    def __poll(cls, context):
         if context.area.spaces.active.type != "TEXT_EDITOR":
-            return False
-        return context.area.spaces.active.text != None
+                return False
+        return context.area.spaces.active.text != None       
 
     def execute(self, context):
         script = context.area.spaces.active.text
         script.write(f'from io_ggltf.Advanced import *\nfrom io_ggltf.Constants import *\n\nbucket = File.create_bucket(filePath="//", fileName="{script.name.replace(".py", "")}", binPath="bin/", fileType=FILE_TYPE_GLB)\nFile.dump_bucket(bucket)\n')
         return {"FINISHED"}
 
+class PasteImportsOnlyOperator(bpy.types.Operator):
+    """Paste required imports"""
+    bl_idname = "ggltf.paste_imports"
+    bl_label = "Paste required imports"
+    bl_button_label = "Imports"
+    bl_button_icon = "FILE_TEXT"
+
+    @classmethod
+    def __poll(cls, context):
+        if context.area.spaces.active.type != "TEXT_EDITOR":
+                return False
+        return context.area.spaces.active.text != None  
+
+    def execute(self, context):
+        script = context.area.spaces.active.text
+        script.write(f'from io_ggltf.Advanced import *\nfrom io_ggltf.Constants import\n')
+        return {"FINISHED"}
+
+class PasteMakeButtonOperator(bpy.types.Operator):
+    """Paste an example button"""
+    bl_idname = "ggltf.paste_make_button"
+    bl_label = "Paste an example button"
+    bl_button_label = "Button"
+    bl_button_icon = "MOUSE_LMB"
+
+    @classmethod
+    def __poll(cls, context):
+        if context.area.spaces.active.type != "TEXT_EDITOR":
+                return False
+        return context.area.spaces.active.text != None   
+
+    def execute(self, context):
+        script = context.area.spaces.active.text
+        script.write('@MakeButton("My Tab/My Panel/My Button")\ndef example_func(self, context):\n\t#Do stuff here\n\treturn {"FINISHED"}')
+        return {"FINISHED"}
+
 classes = [
-    PasteBoilerPlateOperator
+    PasteQuickStartOperator,
+    PasteImportsOnlyOperator,
+    PasteMakeButtonOperator
 ]
 
 def register():
--- Core/BlenderUI/Panels/TextEditorBoilerPlatePanel.py
+++ Core/BlenderUI/Panels/TextEditorBoilerPlatePanel.py
@@ -1,4 +1,5 @@
 import bpy
+from io_ggltf.Core.BlenderUI.Operators import TextEditorBoilerPlateOps as Op
 
 class PasteBoilerPlatePanel(bpy.types.Panel):
     """Panel containing methods that paste object accessors for the user"""
@@ -9,7 +10,9 @@ class PasteBoilerPlatePanel(bpy.types.Panel):
     bl_category = "gglTF"
 
     def draw(self, context):
-        self.layout.row().operator("ggltf.paste_adv_boiler_plate", text="Boiler Plate", icon="FILE_TEXT")
+        self.layout.row().operator(Op.PasteImportsOnlyOperator.bl_idname, text=Op.PasteImportsOnlyOperator.bl_button_label, icon=Op.PasteImportsOnlyOperator.bl_button_icon)
+        self.layout.row().operator(Op.PasteQuickStartOperator.bl_idname, text=Op.PasteQuickStartOperator.bl_button_label, icon=Op.PasteQuickStartOperator.bl_button_icon)
+        self.layout.row().operator(Op.PasteMakeButtonOperator.bl_idname, text=Op.PasteMakeButtonOperator.bl_button_label, icon=Op.PasteMakeButtonOperator.bl_button_icon)
 
 classes = [
     PasteBoilerPlatePanel
