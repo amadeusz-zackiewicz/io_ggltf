cbf993e6478bc063c560081fd42363a5078898ea
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2022-11-15 17:42:10 +0000

    Expose create_filter in blender UI

--- Advanced/Util.py
+++ Advanced/Util.py
@@ -1,6 +1,6 @@
 from io_ggltf import Constants as __c
 from io_ggltf.Core.Bucket import Bucket
-from io_ggltf.Core.Util import create_filter, pattern_replace as __replace
+from io_ggltf.Core.Util import create_filter as __create_filter, pattern_replace as __replace
 from io_ggltf.Core.Decorators import ShowInUI as __ShowInUI
 
 @__ShowInUI(docsURL="https://github.com/amadeusz-zackiewicz/io_ggltf/wiki/Util-Module#pattern_replace_node_names")
@@ -18,3 +18,7 @@ def pattern_replace_all_names(bucket: Bucket, pattern: str, replacement: str):
     """Replace all mesh and node names based on the pattern"""
     pattern_replace_node_names(bucket, pattern, replacement)
     pattern_replace_mesh_names(bucket, pattern, replacement)
+
+@__ShowInUI()
+def create_filter(pattern: str, whitelist=True):
+    return __create_filter(pattern=pattern, whitelist=whitelist)
