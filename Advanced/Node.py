amadeusz-zackiewicz/io_ggltf

commit bda9e0274e086b91977718272a412f5d289b6b21

Fix an issue where Node functions could unintentionally add bone attachments to hierarchy

Advanced/Node.py
@@ -71,9 +71,10 @@ def based_on_hierarchy(bucket: Bucket, topObjAccessor, blacklist = {}, parent=No
             childrenIDs.extend(based_on_collection(bucket=bucket, collectionName=get_object_accessor(obj.instance_collection), blacklist=blacklist, parent=True, checkRedundancies=checkRedundancies))
         else:
             for c in obj.children:
-                childID = __recursive(bucket, c, blacklist, True, checkRedundancies, filters, autoAttachData, True)
-                if childID != None:
-                    childrenIDs.append(childID)
+                if obj.type == __c.BLENDER_TYPE_ARMATURE and c.parent_type == __c.BLENDER_TYPE_OBJECT: # ignore children that belong to the armature of the current object
+                    childID = __recursive(bucket, c, blacklist, True, checkRedundancies, filters, autoAttachData, True)
+                    if childID != None:
+                        childrenIDs.append(childID)
 
         if checkRedundancies:
             redundant, nodeID = RM.register_unique(bucket, get_object_accessor(obj), __c.BUCKET_DATA_NODES)

Constants.py
@@ -121,6 +121,7 @@ BLENDER_TYPE_CURVE          = "CURVE"
 BLENDER_TYPE_EMPTY          = "EMPTY"
 BLENDER_TYPE_SURFACE        = "SURFACE"
 BLENDER_TYPE_MESH           = "MESH"
+BLENDER_TYPE_OBJECT         = "OBJECT"
 
 # General
 BLENDER_AREA_VIEW_3D            = "VIEW_3D"

Tests/expected_output/rigify_rig_ignore_attachments.gltf
@@ -1,0 +1,20 @@
+{
+    "asset": {
+        "generator": "Blender ggltf",
+        "version": "2.0"
+    },
+    "scenes": [
+        {
+            "name": "scene",
+            "nodes": [
+                0
+            ]
+        }
+    ],
+    "nodes": [
+        {
+            "name": "rigify_rig"
+        }
+    ],
+    "scene": 0
+}

Tests/expected_output/rigify_rig_ignore_attachments_embedded.gltf
@@ -1,0 +1,20 @@
+{
+    "asset": {
+        "generator": "Blender ggltf",
+        "version": "2.0"
+    },
+    "scenes": [
+        {
+            "name": "scene",
+            "nodes": [
+                0
+            ]
+        }
+    ],
+    "nodes": [
+        {
+            "name": "rigify_rig"
+        }
+    ],
+    "scene": 0
+}

Tests/files/rigify_rig/ignore_attachments.py
@@ -1,0 +1,33 @@
+# when a script is run in blender it is considered to be __main__ but
+# when we run this as a file using terminal there is only 1 argument and
+# when blender launches a python file it has 2 arguments
+# so if there is only 1 argument, we are running it as a standalone python file
+# otherwise blender is running it
+import sys
+if __name__ == "__main__" and len(sys.argv) == 1:
+    import os
+    sys.path.append(os.path.abspath(""))
+    import Tests.LimitedTestUtil as T
+    T.run_test("rigify_rig", "rigify_rig", os.path.basename(__file__).replace(".py", ""))
+else:
+    from io_ggltf.Advanced import *
+    from io_ggltf.Constants import *
+
+    filePath = "//..\\..\\output\\"
+    fileName = "rigify_rig_ignore_attachments"
+    binPath = ""
+
+    def test(bucket):
+        Settings.set_setting(bucket, BUCKET_SETTING_NODE_AUTO_ATTACH_DATA, False)
+        Settings.set_setting(bucket, BUCKET_SETTING_MESH_AUTO_ATTACH, False)
+        Settings.set_setting(bucket, BUCKET_SETTING_SKIN_AUTO_ATTACH, False)
+        Node.based_on_hierarchy(bucket, "rigify_rig")
+        #Skin.based_on_object(bucket, ("rigify_rig", r"//..\..\lib_files\rigify_human_generic.blend"), rigifyFlags=RIGIFY_INCLUDE_ROOT | RIGIFY_INCLUDE_DEFORMS)
+        File.dump_bucket(bucket)
+
+    print("---------- Start gltf embedded")
+    test(File.create_bucket(filePath, fileName + "_embedded", binPath, FILE_TYPE_GLTF_EMBEDDED))
+    print("---------- Start gltf with external binaries")
+    test(File.create_bucket(filePath, fileName, binPath, FILE_TYPE_GLTF))
+    print("---------- Start glb")
+    test(File.create_bucket(filePath, fileName, binPath, FILE_TYPE_GLB))

commit 816f3b2cf676591e1603c0454e8daba15ade86d3

Fix node hierarchy issue caused in bda9e02 commit

Advanced/Node.py
@@ -71,10 +71,11 @@ def based_on_hierarchy(bucket: Bucket, topObjAccessor, blacklist = {}, parent=No
             childrenIDs.extend(based_on_collection(bucket=bucket, collectionName=get_object_accessor(obj.instance_collection), blacklist=blacklist, parent=True, checkRedundancies=checkRedundancies))
         else:
             for c in obj.children:
-                if obj.type == __c.BLENDER_TYPE_ARMATURE and c.parent_type == __c.BLENDER_TYPE_OBJECT: # ignore children that belong to the armature of the current object
-                    childID = __recursive(bucket, c, blacklist, True, checkRedundancies, filters, autoAttachData, True)
-                    if childID != None:
-                        childrenIDs.append(childID)
+                if obj.type == __c.BLENDER_TYPE_ARMATURE and c.parent_type == __c.BLENDER_TYPE_BONE: # ignore children that belong to the armature of the current object
+                    continue
+                childID = __recursive(bucket, c, blacklist, True, checkRedundancies, filters, autoAttachData, True)
+                if childID != None:
+                    childrenIDs.append(childID)
 
         if checkRedundancies:
             redundant, nodeID = RM.register_unique(bucket, get_object_accessor(obj), __c.BUCKET_DATA_NODES)