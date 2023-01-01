3912f1723e0a776d8b0ae65b668602473d51e5aa
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2023-01-01 09:15:59 +0000

    Make sure AnimationDescriber variables are passed correctly

--- Advanced/Animation.py
+++ Advanced/Animation.py
@@ -5,7 +5,7 @@ from io_ggltf.Core.Scoops.Animation import Animation
 
 @__ShowInUI()
 def create_describer(name: str, frameStart=None, frameEnd=None, frameStep=None, useSteppedInterpolation=None, optimiseKeys=None):
-    return AnimationDescriber(name=name, frameStart=frameStart, frameEnd=frameEnd)
+    return AnimationDescriber(name=name, frameStart=frameStart, frameEnd=frameEnd, frameStep=frameStep, useSteppedInterpolation=useSteppedInterpolation, optimiseKeys=optimiseKeys)
 
 @__ShowInUI()
 def add(bucket, animDescriber: AnimationDescriber):
