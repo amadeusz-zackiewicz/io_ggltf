e95bac94bb257c6a474ae4679f2936e3a654ee44
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2023-01-01 08:21:29 +0000

    Update version to 0.2.0

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     'name': 'gglTF (Game glTF) Exporter',
     'author': 'Amadeusz Zackiewicz',
-    "version": (0, 1, 0),
+    "version": (0, 2, 0),
     'blender': (3, 3, 0),
     'location': 'Text Editor',
     'description': 'Export glTF 2.0 files and enjoy time saving functionality via python scripts',
