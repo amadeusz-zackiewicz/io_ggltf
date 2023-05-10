307e7298efbe0a4069feb5c0e05f9c4838562cf3
DESKTOP-6PITPE2\amade <amadeusz.zackiewicz@gmail.com>
2023-05-10 22:21:08 +0100

    Fix issues with AnimationDescriber hierarchy scan

--- Core/Util.py
+++ Core/Util.py
@@ -233,16 +233,17 @@ def get_all_nodes_in_hierarchy(bucket, topNodeID):
         if not __c.NODE_CHILDREN in node:
             return
         for c in node[__c.NODE_CHILDREN]:
+
             if c in nodes:
                 continue
-            add_children[bucket, bucket.data[__c.BUCKET_DATA_NODES][c], nodes]
+            add_children(bucket, bucket.data[__c.BUCKET_DATA_NODES][c], nodes)
             nodes.add(c)
 
     nodes = set()
     node = bucket.data[__c.BUCKET_DATA_NODES][topNodeID]
+    nodes.add(topNodeID)
 
     add_children(bucket, node, nodes)
-
     return nodes
 
 def get_yup_transforms(childAccessor, parent):
