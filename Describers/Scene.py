51c8ba48918497a7851fea936783091de35892c2
Amadeusz Zackiewicz <amadeusz.zackiewicz@gmail.com>
2025-04-28 11:37:21 +0100

    Add Scene describer

---
+++ Describers/Scene.py
@@ -0,0 +1,32 @@
+from io_ggltf import Constants as C
+from io_ggltf.Describers.Base import Describer
+from io_ggltf.Describers.Node import NodeDescriber
+
+class Scene(Describer):
+	def __init__(self):
+		super().__init__()
+
+		self._dataTypeHint = C.GLTF_SCENE
+
+		self._nodes: list[NodeDescriber]
+
+	def append_node(self, node: NodeDescriber):
+		if not self._isExported:
+			self._nodes.append(node)
+		else:
+			print("Attempted to append a node to scene that is already exported.")
+
+	def _export(self, isBinary, gltfDict, fileTargetPath):
+		if not self._isExported:
+			self._export_name()
+
+			if len(self._nodes) > 0:
+				nodeIDs = []
+
+				for node in self._nodes:
+					nodeIDs.append(node._get_id_reservation())
+
+				self._exportedData[C.SCENE_NODES] = nodeIDs
+
+		else:
+			print("Attempted to export a scene that is already exported.")
