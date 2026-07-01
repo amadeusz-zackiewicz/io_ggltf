e4396bece4e17c1ab0cae70cb165fae553780c70
amadeusz-zackiewicz <amadeusz.zackiewicz@gmail.com>
2026-07-01 23:32:54 +0100

    Extensions: rename __init__ functions

--- Extensions/__init__.py
+++ Extensions/__init__.py
@@ -4,14 +4,14 @@ from .KHR_animation_pointer import KHRAnimationPointers
 from .GGLTF_node_extra_properties import NodeBoolean as NodeBooleanProperty
 from .GODOT_single_root import GodotSingleRoot
 
-def ext_godot_single_root(file):
+def add_godot_single_root(file):
 	return GodotSingleRoot(file, file)
 
-def ext_animation_pointers(file, animation):
+def add_animation_pointers(file, animation):
 	return KHRAnimationPointers(file, animation, kwargs={})
 
-def ext_node_visibility(file, node, enableAnimation: bool = True):
+def add_node_visibility(file, node, enableAnimation: bool = True):
 	return KHRNodeVisibility(file, node, enableAnimation, kwargs={})
 
-def ext_node_boolean_property(file, node, objProperty: str, exportName: str, enableAnimation: bool = True, addVariableMetaData: bool = False):
+def add_node_property_boolean(file, node, objProperty: str, exportName: str, enableAnimation: bool = True, addVariableMetaData: bool = False):
 	return NodeBooleanProperty(file, node, objProperty, exportName, addVariableMetaData, enableAnimation)
